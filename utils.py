b5a091dce804359972904baa26703fed57cd418f
SunilBani <sunilbani31@gmail.com>
2025-04-12 19:00:23 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,42 @@
+import re
+import string
+
+def calculate_password_strength(password):
+    score = 0
+    feedback = []
+
+    if len(password) >= 8:
+        score += 1
+    else:
+        feedback.append("Password is too short (minimum 8 characters).")
+
+    if re.search(r"[a-z]", password):
+        score += 1
+    else:
+        feedback.append("Add lowercase letters.")
+
+    if re.search(r"[A-Z]", password):
+        score += 1
+    else:
+        feedback.append("Add uppercase letters.")
+
+    if re.search(r"\d", password):
+        score += 1
+    else:
+        feedback.append("Add numbers.")
+
+    if re.search(rf"[{re.escape(string.punctuation)}]", password):
+        score += 1
+    else:
+        feedback.append("Add special characters (!@#$ etc.).")
+
+    strength = "Weak"
+    if score == 5:
+        strength = "Very Strong"
+    elif score == 4:
+        strength = "Strong"
+    elif score == 3:
+        strength = "Medium"
+
+    return strength, feedback
 
